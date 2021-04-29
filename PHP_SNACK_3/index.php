1897a6840284333c840a107acde36fae1a719910
wvwall <waltervelardo@gmail.com>
2021-04-29 17:39:22 +0200

    Completed s3

--- PHP_SNACK_3/index.php
+++ PHP_SNACK_3/index.php
@@ -5,11 +5,11 @@ $arrayPost = [
 
         [
             "title" => "Post 1",
-            "author" => "Autore 1",
+            "author" => "Aldo",
         ],
         [
             "title" => "Post 2",
-            "author" => "Autore 2",
+            "author" => "Giovanni",
         ]
     ],
 
@@ -17,40 +17,48 @@ $arrayPost = [
         
         [
             "title" => "Post 1",
-            "author" => "Autore 1",
+            "author" => "Giacomo",
         ],
         [
             "title" => "Post 2",
-            "author" => "Autore 2",
+            "author" => "Al",
         ]
     ],
     "03/03/2021" => [
         
         [
             "title" => "Post 1",
-            "author" => "Autore 1",
+            "author" => "Jonh",
         ],
         [
             "title" => "Post 2",
-            "author" => "Autore 2",
+            "author" => "Jack",
         ]
     ],
     "04/04/2021" => [
         
         [
             "title" => "Post 1",
-            "author" => "Autore 1",
+            "author" => "Wall",
         ],
         [
             "title" => "Post 2",
-            "author" => "Autore 2",
+            "author" => "Walter",
         ]
     ],
  
 ];
 
-for ($i=0; $i < count($arrayPost) ; $i++) { 
-    echo $arrayPost["01/01/2021"]["title"];
+foreach ($arrayPost as $key => $value) { ?>
+    <div> <?= $key ?> </div>
+<?php
+
+    for ($i=0; $i < count($value) ; $i++) { ?>
+        <div>Titolo: <?= $value[$i]['title'] ?></div>
+        <div>Autore: <?= $value[$i]['author'] ?></div>  
+        <?php
+    }
+
 }
 
 ?>
