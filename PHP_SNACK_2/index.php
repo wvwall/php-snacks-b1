f8ecfc30c3231032d68e99dc46a11988776759e3
wvwall <waltervelardo@gmail.com>
2021-04-30 10:25:58 +0200

    Completed

--- PHP_SNACK_2/index.php
+++ PHP_SNACK_2/index.php
@@ -9,7 +9,7 @@ $validEmail_at = strpos($email, "@");
 $validEmail_dot = strpos($email, ".");
 $validAge = filter_var($age, FILTER_VALIDATE_INT);
 
-if ($validName > 3 && $validEmail_at && $validEmail_dot && $validAge ) {
+if ($validName > 3 && $validEmail_at !== false && $validEmail_dot !== false && $validAge ) {
     echo $name ."<br>";
     echo $email ."<br>";
     echo $age ."<br>";
--- PHP_SNACK_4/index.php
+++ PHP_SNACK_4/index.php
@@ -2,20 +2,20 @@
 
     $studenti = [
          [
-            "Nome" => "nome1",
-            "Cognome" => "cognome1",
+            "Nome" => "Aldo",
+            "Cognome" => "Baglio",
             "Voti" => [1,2,3,4,5]
         ],
 
         [
-            "Nome" => "nome2",
-            "Cognome" => "cognome2",
+            "Nome" => "Giovanni",
+            "Cognome" => "Storti",
             "Voti" => [2,3,4,5,6]
         ],
 
          [
-            "Nome" => "nome3",
-            "Cognome" => "cognome3",
+            "Nome" => "Giacomo",
+            "Cognome" => "Poretti",
             "Voti" => [3,4,5,6,7]
         ],
     ];
@@ -24,7 +24,7 @@
         <div>
             <?= $studenti[$i]["Nome"]  ?>
             <?= $studenti[$i]["Cognome"] ?>
-            <?= "Media Pagella: ".$media = array_sum($studenti[$i]["Voti"])/count($studenti[$i]["Voti"])  ?>
+            <?= "Media Pagella: ".$media = array_sum($studenti[$i]["Voti"]) / count($studenti[$i]["Voti"])  ?>
         </div>
         <?php   
     }
